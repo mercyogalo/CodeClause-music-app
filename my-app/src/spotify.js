fb34d9bfec92a0c0b1d30c50d397d24c3ec41751
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2024-05-26 15:13:39 +0300

    changed the titile and made it Seo friendly

--- my-app/src/spotify.js
+++
@@ -1,7 +0,0 @@
-const authEndpoint="https://accounts.spotify.com/authorize?";
-const clientID="f53166d8986c455fadc8b5abf861cfdc";
-const redirectUrl="http://localhost:3000";
-const scope=["user-library","playlist-read-private"];
-
-
-export  const loginEndpoint=`${authEndpoint}client_id=${clientID}&redirect_url=${ redirectUrl}&scopes=${scope.join("%20")}&response_type=token&show_dialog=true`;
