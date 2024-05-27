62ed9723219caa289bd54b85926dcbd3828c5f8d
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2024-05-27 16:40:22 +0300

    removed the screens and started over

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import react from "react";
-import Home from "./Screens/Home"
+
 
 
 export default function App(){
   return(
-<div className="App">
-  <Home />
+<div className="container">
+  <img src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/126/467/original/Ariana_Grande_Autographed_Signed_%E2%80%A2_Ad%E2%80%A2.jpeg?1715706089" class="img-fluid rounded-circle" alt="" />
 </div>
   );
 }
