ab9052b289ac1755fdb44c4f51544ef1794f3d2d
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2024-05-27 09:21:24 +0300

    created the screens folder with each screen element

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Auth from "./App"
+import App from "./App"
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Auth />
+    <App />
   </React.StrictMode>
 );
 
